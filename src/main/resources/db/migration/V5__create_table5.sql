12d35709b18daa0613d3390d7d68df0bfd0fdf0e
zhaozhentao <qq344696734@gmail.com>
2022-01-26 07:45:13 +0800

    flyway

---
+++ src/main/resources/db/migration/V5__create_table5.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `table3` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
